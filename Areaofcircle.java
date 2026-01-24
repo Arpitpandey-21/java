b09892abb016845ba4db44ab0471b28220f12c0e
arpit pandey <arpitpandit892855@gmail.com>
2026-01-24 23:42:59 +0530

    git add

---
+++ Areaofcircle.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Areaofcircle {
+    static void main() {
+        double a;
+        double r = 5;
+        a=3.14*r*r;
+        System.out.println(area);
+    }
+}
+//Area of circle=pi*r*r
---
+++ VolumeOfSphere.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class VolumeOfSphere {
+    static void main() {
+        double v;
+        double r=5;
+        v = (4.0 / 3.0) * 3.14 * r * r * r;
+        System.out.println("Volume: " + v);
+    }
+}
