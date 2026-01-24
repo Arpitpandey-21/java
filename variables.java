3e9825512480d608c9c5e585b335cbbdec4c2660
arpit pandey <arpitpandit892855@gmail.com>
2026-01-24 14:35:12 +0530

    git add

---
+++ variables.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class variables {
+    static void main(){
+        int x=5;
+        System.out.println(x);
+        int y; //declaration
+        y=200; //initialization
+        System.out.println(y);
+        System.out.println(x*y);
+    }
+}
+//if you give double inverted comma in print and println you get output in from of text or string
