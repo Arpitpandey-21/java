8c0831967f04dd8b782d763d175d91f61646500d
arpit pandey <arpitpandit892855@gmail.com>
2026-01-24 23:12:32 +0530

    git add

---
+++ ArithmaticOperations.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class ArithmaticOperations {
+    static void main() {
+        int x,y;
+        x=10;
+        y=20;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x%y);
+    }
+}
