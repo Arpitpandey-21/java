4e319f266db7c8500a2d5eb90630bd4663437630
arpit pandey <arpitpandit892855@gmail.com>
2026-01-25 00:14:10 +0530

    git add

--- Areaofcircle.java
+++ Areaofcircle.java
@@ -1,11 +1,16 @@
 package basic;
 
+import java.util.Scanner;
+
 public class Areaofcircle {
     static void main() {
+        Scanner sc = new Scanner(System.in); //for taking inputs
+        System.out.print("Enter radius: ");
+        double r = sc.nextDouble();
         double a;
-        double r = 5;
         a=3.14*r*r;
-        System.out.println("Area: " +a);
+        System.out.print("Area of circle=");
+        System.out.println(a);
     }
 }
 //Area of circle=pi*r*r
